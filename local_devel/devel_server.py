bab0dc7f729185dff28f862379ac8a622c278d4b
Joel Braun <joel.m.braun33@gmail.com>
2018-04-15 21:14:11 -0400

    Local development for testing

---
+++ local_devel/devel_server.py
@@ -0,0 +1,25 @@
+import flask
+
+app = flask.Flask('app_main', static_url_path='')
+
+FILE_JSON = 'files.json'
+SCAN_JSON = 'scan.json'
+
+
+@app.route('/api/1/files')
+def test_files():
+    return open(FILE_JSON).read()
+
+
+@app.route('/api/1/files/scans')
+def test_scans():
+    return open(SCAN_JSON).read()
+
+
+@app.route('/api/1/user/loggedin')
+def test_logged_in():
+    return flask.jsonify({'loggedin': 1})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 8080, debug=True)
